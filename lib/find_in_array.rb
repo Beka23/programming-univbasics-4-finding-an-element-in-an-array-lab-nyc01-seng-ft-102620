903060ac0766fa0912f311a235c3b8849bead57e
Beksultan <beksultankaiypov@gmail.com>
2020-10-08 21:48:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
  counter = 0
   while counter < array.length do
+    if array[count] == value_to_find
    puts array[count] == value_to_find
     count += 1
   end
